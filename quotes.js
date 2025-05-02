039e75d9eda0667102cb52ecf7f01061037e9bbb
Mariami-amb12 <mariami.ambroladze881@cve.tsu.edu.ge>
2025-05-02 16:01:25 +0400

    add new quotes

--- quotes.js
+++ quotes.js
@@ -551,4 +551,8 @@ const localQuotes = [
     text: "You can do what's reasonable or you can decide what's possible.",
     author: null,
   },
+  {
+    text: "loedadada dad",
+    author: null,
+  },
 ];
